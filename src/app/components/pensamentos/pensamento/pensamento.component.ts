51142b369ce524cbb6cdfffe9a263d78d8c8d33a
joneilson <joneilson84@gmail.com>
2023-01-29 20:42:35 -0300

    finalização da funcionalidade editar e excluir

---
+++ src/app/components/pensamentos/pensamento/pensamento.component.ts
@@ -0,0 +1,26 @@
+import { Pensamento } from './../pensamento';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-pensamento',
+  templateUrl: './pensamento.component.html',
+  styleUrls: ['./pensamento.component.css']
+})
+export class PensamentoComponent {
+  @Input() pensamento: Pensamento = {
+    id: 0,
+    conteudo: 'I love Angular',
+    autoria: 'Nay',
+    modelo: 'modelo3'
+  }
+
+  constructor(){}
+
+  larguraPensamento(): string {
+    if(this.pensamento.conteudo.length >= 256) {
+      return 'pensamento-g'
+    }
+    return 'pensamento-p'
+  }
+
+}
